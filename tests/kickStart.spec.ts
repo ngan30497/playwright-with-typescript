27a3d54dc941c8632640ca1667bb4cdab21bf779
kimngan <ntkngan30497@gmai.com>
2025-10-05 12:47:04 +0700

    setup & samle tc

---
+++ tests/kickStart.spec.ts
@@ -0,0 +1,8 @@
+import { test } from "@playwright/test"
+test("kick start with playwright", () => {
+    console.log("My First Test")
+})
+
+test("My 2nd test script", () => {
+    console.log("2nd test script")
+})
